1b2d0357b8f4c7eaa1476918b12615fe8be2c977
MathieuEDR <135825957+MathieuEDR@users.noreply.github.com>
2026-03-03 15:51:24 +0000

    Refactor everything (#1)

---
+++ MessageBusReader/DataTypes/ErrorQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MessageBusReader.DataTypes;
+
+internal enum ErrorQueue
+{
+    Unknown,
+    General,
+    Order,
+    Product,
+    Ballot
+}
+
+internal static class ErrorQueueEnumExtension
+{
+    internal static string GetQueueName(this ErrorQueue errorQueue)
+    {
+        return errorQueue switch
+        {
+            ErrorQueue.General => "error",
+            ErrorQueue.Order => "error_order",
+            ErrorQueue.Product => "error_product",
+            ErrorQueue.Ballot => "error_ballot",
+            _ => throw new ArgumentOutOfRangeException(nameof(errorQueue), errorQueue, null)
+        };
+    }
+
+    internal static ErrorQueue ToErrorQueue(this string errorQueue)
+    {
+        return errorQueue switch
+        {
+            "error" => ErrorQueue.General,
+            "error_order" => ErrorQueue.Order,
+            "error_product" => ErrorQueue.Product,
+            "error_ballot" => ErrorQueue.Ballot,
+            _ => throw new ArgumentOutOfRangeException(nameof(errorQueue), errorQueue, null)
+        };
+    }
+}
--- MessageBusReader/Program.cs
+++ MessageBusReader/Program.cs
@@ -1,417 +1,125 @@
-﻿namespace MessageBusReader
+﻿using System.Collections.Generic;
+using MessageBusReader.Configuration;
+using MessageBusReader.DataTypes;
+using MessageBusReader.Services;
+
+namespace MessageBusReader;
+
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+internal static class Program
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Newtonsoft.Json.Linq;
-    using Azure.Messaging.ServiceBus;
-    using System.Threading;
-    using System.IO;
+    private static TaskCompletionSource<int> _taskCompletionSource;
+    private static Task<int> _loopTask;
 
-    class Program
+    static async Task Main(string[] args)
     {
-        private static ServiceBusClient _client;
-        private static ServiceBusProcessor _processor;
-
-        private static TaskCompletionSource<int> _taskCompletionSource;
-        private static Task<int> _loopTask;
-        private static int _completeCounter = 0;
-        private static int _returnCounter = 0;
-        private static int _messageCount = 0;
-        private static int _delay = 0;
-
-        private static IDictionary<string, ServiceBusSender> _senders = new Dictionary<string, ServiceBusSender>();
-
-        static async Task Main(string[] args)
+        var serviceBusClientBuilder = new ServiceBusClientBuilder();
+        var client = serviceBusClientBuilder.BuildServiceBusClient();
+        var inputs = new ExecutionInputConfiguration()
         {
-            var root = Directory.GetCurrentDirectory();
-            var dotenv = Path.Combine(root, ".env");
-            DotEnv.Load(dotenv);
+            TargetErrorQueue = ErrorQueue.General,
+            MessageTypesToDeleteWithoutAction =
+            [
+                // "Edrington.Contracts.Ecommerce.Events.CheckoutCompleted, Edrington.Contracts.Ecommerce"
+            ],
+            MessageTypesToReplay =
+            [
+                // "Edrington.Contracts.Orders.Events.OrderRefreshFromShopDownloadedV2, Edrington.Contracts.Orders"
+                // "Edrington.Contracts.Ecommerce.Events.CheckoutCompleted, Edrington.Contracts.Ecommerce",
+                "Edrington.Data.Consumer.Tiering.SetTieringModel, Edrington.Data",
+                "Edrington.Data.Consumer.Tiering.SetConsumerTierAtCrm, Edrington.Data"
+            ],
+            MessagesToCollectDataFrom =
+            [
+                // "Edrington.Contracts.Ecommerce.Events.CheckoutCompleted, Edrington.Contracts.Ecommerce"
+            ],
+            DeadLetterEverythingElse = false,
+            CollectCountByMessageType = false,
+        };
 
-            string env;
-            env = "PRODUCTION_CONNECTION_STRING";
-            // env = "QA_CONNECTION_STRING";
-            // env = "DEV_CONNECTION_STRING";
-            var connectionString = Environment.GetEnvironmentVariable(env);
-
-            // Connect to error queue
-            _client = new ServiceBusClient(connectionString);
-
-            // await MainAsync();
-
-            // Switch to this to move deadletter back to the error queue
-            await MoveDeadletter();
-        }
-
-        static async Task MainAsync()
-        {
-            ServiceBusProcessorOptions options = new ServiceBusProcessorOptions
+        List<COnditionAction> ExecutionConfig =
+        [
+            new COnditionAction()
             {
-                AutoCompleteMessages = false,
-                MaxConcurrentCalls = 1,
-                ReceiveMode = ServiceBusReceiveMode.PeekLock,
-            };
-
-            _processor = _client.CreateProcessor("error", options);
-
-            _processor.ProcessMessageAsync += ProcessMessagesAsync;
-            _processor.ProcessErrorAsync += ExceptionReceivedHandler;
-
-            _taskCompletionSource = new TaskCompletionSource<int>();
-            _loopTask = _taskCompletionSource.Task;
-
-            await _processor.StartProcessingAsync();
-
-            await _loopTask;
-
-            Console.WriteLine("Task finished");
-
-            Console.ReadLine();
-        }
-
-        private static async Task ProcessMessagesAsync(ProcessMessageEventArgs args)
-        {
-            // TODO: Let's get a more general location for any log files.
-            var logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "OneDrive - Edrington", "Documents", "Support\bot-attack-february-2024");
-            var logFilename = Path.Combine(logFilePath, "Log.txt");
-            var errorFilename = Path.Combine(logFilePath, "Errors.txt");
-
-            ServiceBusReceivedMessage message = args.Message;
-            var body = Encoding.UTF8.GetString(message.Body);
-            JObject json;
-
-            try
-            {
-                json = JObject.Parse(body);
+                Condition = message => true,
+                Action = () => Console.WriteLine("action")
             }
-            catch (Exception)
-            {
-                File.AppendAllText(errorFilename, args.Message.Subject);
-                File.AppendAllText(errorFilename, "\n");
-                throw;
-            }
+        ];
 
-            _messageCount = Interlocked.Increment(ref _messageCount);
+        var messageProcessor = new MessageProcessor(new ServiceBusSenderCache(client), inputs);
 
-            Console.WriteLine($"Processing {_messageCount}: {message.MessageId}");
+        await ProcessMessages(inputs.TargetErrorQueue, messageProcessor, client);
 
-            if (message.ApplicationProperties.TryGetValue("rbs2-msg-type", out object typeValue) == false)
-            {
-                Console.WriteLine("Skipping message as unable to get message type");
-                return;
-            }
+        // Switch to this to move deadletter back to the error queue
+        // await ReturnToDeadLetter(inputs.TargetErrorQueue, messageProcessor, client);
 
-            string type = typeValue.ToString();
+        Console.WriteLine("Execution finished");
 
-            // SynchroniseConsentPreferences
-            if (type == "Edrington.Data.Consumer.Commands.SynchroniseConsentPreferences, Edrington.Data")
-            {
-                if (message.ContainsError("Unable to find consumer for consent manager id"))
-                {
-                    await CompleteMessage(args);
-                    return;
-                }
-            }
-
-            // ShopProductIdentityUpserted
-            if (type == "Edrington.Contracts.Ecommerce.Events.ShopProductIdentityUpserted, Edrington.Contracts.Ecommerce")
-            {
-                var brand = json.GetValue("brand").Value<int>();
-
-                if (brand == 1)
-                {
-                    // Don't need this for Macallan any more 
-                    await CompleteMessage(args);
-                    return;
-                }
-            }
-
-            // CreateSkeletonContact
-            if (type == "Edrington.Data.Consumer.Commands.CreateSkeletonContact, Edrington.Data")
-            {
-                var brand = json.GetValue("brand").Value<int>();
-
-                if (brand == 1)
-                {
-                    await CompleteMessage(args);
-                    return;
-                }
-
-                if (message.ContainsError("Invalid email for contact"))
-                {
-                    await CompleteMessage(args);
-                    return;
-                }
-            }
-
-            // DeleteConsentManagementRecord
-            if (type == "Edrington.Data.Consumer.Commands.DeleteConsentManagementRecord, Edrington.Data")
-            {
-                if (message.ContainsError("Status Code: GatewayTimeout"))
-                {
-                    await ReturnToSource(args, _delay);
-                    _delay++;
-                    return;
-                }
-            }
-
-            // ProductInterestRegistered (bots) 
-            if (type == "Edrington.Contracts.Ecommerce.Events.ProductInterestRegistered, Edrington.Contracts.Ecommerce")
-            {
-                var botScore = json.GetValue("BotScore").Value<double>();
-                if (botScore < 0.4)
-                {
-                    await CompleteMessage(args);
-                    return;
-                }
-
-                await ReturnToSource(args, _delay);
-                _delay++;
-                return;
-            }
-
-            // AuthenticationUserDeleted - identity deleted
-            if (type == "Edrington.Data.Authentication.Events.AuthenticationUserDeleted, Edrington.Data")
-            {
-                if (message.ContainsError("Status (404)") && message.ContainsError("_DELETED_"))
-                {
-                    // delete
-                    await CompleteMessage(args);
-                    return;
-                }
-
-                await ReturnToSource(args, _delay);
-                _delay++;
-                return;
-            }
-
-            // 429s - let's try again
-            if (message.ContainsError("Status (429) Too Many Requests"))
-            {
-                await ReturnToSource(args, _delay);
-                _delay++;
-                return;
-            }
-
-            // These are the messages we are going to remove from the queue without doing anything
-            string[] completeTypes =
-            {
-                "Edrington.Data.CrmBridge.Commands.SyncEventDeal, Edrington.Data",
-                "Edrington.Data.CrmBridge.Commands.SyncPurchase, Edrington.Data",
-                "Edrington.Data.CrmBridge.Commands.SyncPurchases, Edrington.Data"
-            };
-
-            if (completeTypes.Contains(type))
-            {
-                await CompleteMessage(args);
-                return;
-            }
-
-            // These are the messages we are going to try to replay 
-            string[] returnTypes =
-            {
-                "Edrington.Data.Consumer.Commands.AccountSignUpForSubscriberRequested, Edrington.Data",
-                "Edrington.Data.Consumer.Commands.MarkConsumerEmailVerified, Edrington.Data",
-                "Edrington.Data.Consumer.Events.ConsumerProfileUpdated, Edrington.Data",
-                "Edrington.Data.Consumer.Events.ConsumerVerifiedEmail, Edrington.Data",
-                "Edrington.Data.ProductInventoryAlert.Events.PurchaseObjectInserted, Edrington.Data"
-            };
-
-            if (returnTypes.Contains(type))
-            {
-                await ReturnToSource(args, _delay);
-                _delay += 5;
-                return;
-            }
-
-            // Capture 404 for... 
-            if (type == "Edrington.Data.Order.Events.OrderRefreshFromShopDownloadedV2, Edrington.Data")
-            {
-                if (message.ContainsError("Request to the Product API v2 failed with unsuccessful status code 404"))
-                {
-                    var errorMessage = message.GetErrorMessage();
-                    var startOfInterestingBit = errorMessage.IndexOf("Url: https://api.edrington.com");
-                    var endOfInterestingBit = errorMessage.IndexOf(")");
-                    var interestingBit = errorMessage.Substring(startOfInterestingBit, endOfInterestingBit - startOfInterestingBit + 1);
-
-                    File.AppendAllText(logFilename, interestingBit);
-                    File.AppendAllText(logFilename, "\n");
-                }
-            }
-
-            // And we move anything else to DLQ
-            await args.DeadLetterMessageAsync(args.Message);
-            return;
-        }
-
-        static async Task MoveDeadletter()
-        {
-            ServiceBusProcessorOptions options = new ServiceBusProcessorOptions
-            {
-                AutoCompleteMessages = false,
-                MaxConcurrentCalls = 1, // ReturnToQueue code does not support concurrent calls yet.
-                ReceiveMode = ServiceBusReceiveMode.PeekLock,
-                SubQueue = SubQueue.DeadLetter
-            };
-
-            string queueName = "error";
-
-            _processor = _client.CreateProcessor(queueName, options);
-
-            _processor.ProcessMessageAsync += args => ReturnDeadletterAsync(args, queueName);
-            _processor.ProcessErrorAsync += ExceptionReceivedHandler;
-
-            _taskCompletionSource = new TaskCompletionSource<int>();
-            _loopTask = _taskCompletionSource.Task;
-
-            await _processor.StartProcessingAsync();
-
-            await _loopTask;
-        }
-
-        private static async Task ReturnDeadletterAsync(ProcessMessageEventArgs args, string queueName)
-        {
-            ServiceBusReceivedMessage message = args.Message;
-
-            _messageCount = Interlocked.Increment(ref _messageCount);
-
-            Console.WriteLine($"Processing {_messageCount}: {message.MessageId}");
-
-            //if (message.ApplicationProperties.TryGetValue("rbs2-msg-type", out object typeValue) == false)
-            //{
-            //    Console.WriteLine("Skipping message as unable to get message type");
-            //    return;
-            //}
-
-            //string type = typeValue.ToString();
-
-            var clone = new ServiceBusMessage(args.Message);
-
-            await ReturnToQueue(clone, queueName, 0);
-            await CompleteMessage(args);
-        }
-
-        private static async Task CompleteMessage(ProcessMessageEventArgs args)
-        {
-            await args.CompleteMessageAsync(args.Message);
-
-            _completeCounter++;
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{_completeCounter} messages completed");
-            Console.ResetColor();
-        }
-
-        private static async Task ReturnToSource(ProcessMessageEventArgs args)
-        {
-            await ReturnToSource(args, 0);
-        }
-
-        private static async Task ReturnToSource(ProcessMessageEventArgs args, int delay)
-        {
-            string source = GetSource(args);
-
-            if (string.IsNullOrEmpty(source))
-            {
-                Console.WriteLine("Message does not have a source queue property");
-                return;
-            }
-
-            var copy = new ServiceBusMessage(args.Message);
-
-            await ReturnToQueue(copy, source, delay);
-
-            await CompleteMessage(args);
-        }
-
-        private static string GetSource(ProcessMessageEventArgs args)
-        {
-            if (args.Message.ApplicationProperties.TryGetValue("rbs2-source-queue", out object sourceValue))
-            {
-                return sourceValue.ToString();
-            }
-
-            return string.Empty;
-        }
-
-        private static async Task ReturnToQueue(ServiceBusMessage message, string queueName, int delay)
-        {
-            ServiceBusSender sender;
-
-            if (_senders.ContainsKey(queueName))
-            {
-                sender = _senders[queueName];
-            }
-            else
-            {
-                sender = _client.CreateSender(queueName);
-
-                _senders.Add(queueName, sender);
-            }
-
-            if (delay > 0)
-            {
-                await sender.ScheduleMessageAsync(message, DateTimeOffset.UtcNow.AddSeconds(delay));
-            }
-            else
-            {
-                await sender.SendMessageAsync(message);
-            }
-
-            _returnCounter++;
-
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine($"{_returnCounter} messages returned");
-            Console.ResetColor();
-        }
-
-        static Task ExceptionReceivedHandler(ProcessErrorEventArgs args)
-        {
-            Console.WriteLine($"Message handler encountered an exception {args.Exception}.");
-            return Task.CompletedTask;
-        }
+        Console.ReadLine();
     }
 
-    public static class MessageExtensions
+
+    static async Task ProcessMessages(ErrorQueue queueName, MessageProcessor messageProcessor, ServiceBusClient client)
     {
-        const string errorKey = "rbs2-error-details";
-
-        public static bool ContainsError(this ServiceBusReceivedMessage message, string error)
+        var options = new ServiceBusProcessorOptions
         {
-            if (message.ApplicationProperties.ContainsKey(errorKey))
-            {
-                string errorMessage = message.ApplicationProperties[errorKey].ToString();
+            AutoCompleteMessages = false,
+            MaxConcurrentCalls = 1,
+            ReceiveMode = ServiceBusReceiveMode.PeekLock,
+        };
 
-                return errorMessage.Contains(error);
-            }
+        var processor = client.CreateProcessor(queueName.GetQueueName(), options);
 
-            return false;
-        }
 
-        public static string GetErrorMessage(this ServiceBusReceivedMessage message)
+        processor.ProcessMessageAsync += messageProcessor.ProcessMessagesAsync;
+        processor.ProcessErrorAsync += messageProcessor.ExceptionReceivedHandler;
+
+        _taskCompletionSource = new TaskCompletionSource<int>();
+        _loopTask = _taskCompletionSource.Task;
+
+        await processor.StartProcessingAsync();
+
+        await _loopTask;
+    }
+
+    static async Task ReturnToDeadLetter(ErrorQueue queueName, MessageProcessor messageProcessor, ServiceBusClient client)
+    {
+        var options = new ServiceBusProcessorOptions
         {
-            if (message.ApplicationProperties.ContainsKey(errorKey))
-            {
-                return message.ApplicationProperties[errorKey].ToString();
-            }
+            AutoCompleteMessages = false,
+            MaxConcurrentCalls = 1, // ReturnToQueue code does not support concurrent calls yet.
+            ReceiveMode = ServiceBusReceiveMode.PeekLock,
+            SubQueue = SubQueue.DeadLetter
+        };
 
-            return string.Empty;
-        }
+        var processor = client.CreateProcessor(queueName.GetQueueName(), options);
 
-        public static string BodyAsString(this ServiceBusReceivedMessage message)
-        {
-            return Encoding.UTF8.GetString(message.Body);
-        }
+        processor.ProcessMessageAsync += messageEvent => messageProcessor.MoveAllDeadLetterMessageBackToMainQueue(messageEvent, queueName);
+        processor.ProcessErrorAsync += messageProcessor
+            .ExceptionReceivedHandler;
 
-        public static dynamic BodyAsDynamic(this ServiceBusReceivedMessage message)
-        {
-            string body = message.BodyAsString();
+        _taskCompletionSource = new TaskCompletionSource<int>();
+        _loopTask = _taskCompletionSource.Task;
 
-            dynamic msg = JObject.Parse(body);
+        await processor.StartProcessingAsync();
 
-            return msg;
-        }
+        await _loopTask;
+    }
+}
+
+internal class COnditionAction
+{
+    public Func<ServiceBusMessage, bool> Condition { get; set; }
+    public Action Action { get; set; }
+}
+
+internal class ServiceBusClientBuilder
+{
+    internal ServiceBusClient BuildServiceBusClient()
+    {
+        return new ServiceBusClient("Endpoint=sb://prodedringtonservicebus.servicebus.windows.net/;SharedAccessKeyName=SharedKey;SharedAccessKey=sHAYVL6e+8TMSSIo0HhFkl1ffQ8YzoIpLsN2OQP0xww=");
     }
 }
